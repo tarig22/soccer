21703d83c5c7ccc24d39165a008b709f9a326f3c
tarig22 <55983151+tarig22@users.noreply.github.com>
2021-08-26 17:58:35 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import requests
+
+def getData():
+    url = requests.get("https://www.as-goal.com/m/").text
+    x = url.find("<div id=\"Today\"")
+    y = url.find("<div id=\"Tomorrow\"")
+    return len(url[x:y])
+
+
+print(getData())
